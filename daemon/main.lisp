f8a602b176892efa3a608711f0cd87aef64fdc87
Florian Margaine <florian@platform.sh>
2020-04-26 00:15:36 +0200

    Add a unix socket to trigger events to reload list of folders

--- daemon/main.lisp
+++ daemon/main.lisp
@@ -11,6 +11,13 @@
 		#:inotify-event-name
 		#:event-pathname/flags)
   (:import-from :sqlite #:with-open-database #:execute-non-query #:execute-to-list)
+  (:import-from :sb-bsd-sockets
+		#:socket
+		#:local-socket
+		#:socket-bind
+		#:socket-listen
+		#:socket-close
+		#:socket-accept)
   (:export #:main))
 
 (in-package :ballish/daemon/main)
@@ -50,28 +57,54 @@
 	     (execute-non-query ,db ,definition))
        ,@body)))
 
+(defmacro with-unix-socket-server ((socket &key path (backlog 100)) &body body)
+  `(let ((,socket (make-instance 'local-socket
+				 :type :stream)))
+     (socket-bind ,socket ,path)
+     (socket-listen ,socket ,backlog)
+     (unwind-protect
+	  (progn ,@body)
+       (socket-close ,socket))))
+
+(defmacro with-ballish-server ((socket) &body body)
+  `(progn
+     (ensure-directories-exist (uiop:xdg-runtime-dir #p"ballish/"))
+     (with-unix-socket-server (,socket
+			       :path ,(namestring
+				       (uiop:xdg-runtime-dir #p"ballish/daemon.sock")))
+       ,@body)))
+
 (defun main ()
   (with-inotify (inotify)
     (with-ballish-database (db)
       (with-source-indexing (source-index source-queue)
-	(iter (for folder in (execute-to-list db "SELECT path FROM folder"))
-	      (add-watches inotify (car folder) source-queue))
+	(let ((folders (get-folders db)))
+	  (iter (for folder in folders)
+		(add-watches inotify folder source-queue))
 
-	(main-loop db inotify source-index source-queue)))))
+	  (with-ballish-server (socket)
+	    (main-loop db inotify socket source-index source-queue folders)))))))
 
-(defun main-loop (db inotify source-index source-queue)
+(defun main-loop (db inotify socket source-index source-queue folders)
   (let ((queue (make-mailbox)))
     (with-waiting-threads ((wait-for-indexing
 			    :arguments (list source-index queue)
 			    :name "Main source index wait indexing")
 			   (wait-for-inotify-event
 			    :arguments (list inotify queue)
-			    :name "Main source index wait inotify"))
+			    :name "Main wait inotify")
+			   (wait-for-client-socket
+			    :arguments (list socket queue)
+			    :name "Main wait client socket"))
       (loop
 	 (let ((message (receive-message queue)))
 	   (typecase message
 	     (inotify-event
-	      (handle-inotify-event inotify message source-queue))))))))
+	      (handle-inotify-event inotify message source-queue))
+
+	     (socket
+	      (setf folders
+		    (handle-client-socket message db inotify source-queue folders)))))))))
 
 (defun add-watches (inotify path source-queue)
   ;; TODO: don't watch on read-only files (either fs or permission)
@@ -101,6 +134,10 @@
   (loop
      (send-message queue (read-event inotify))))
 
+(defun wait-for-client-socket (socket queue)
+  (loop
+     (send-message queue (socket-accept socket))))
+
 (defun handle-inotify-event (inotify event source-queue)
   (let* ((mask (inotify-event-mask event))
 	 (path (event-pathname/flags inotify event)))
@@ -123,3 +160,16 @@
 
 	  ;; TODO: add :move-from/:move-to
 	  )))
+
+(defun get-folders (db)
+  (mapcar #'car (execute-to-list db "SELECT path FROM folder")))
+
+(defun handle-client-socket (socket db inotify source-queue folders)
+  (unwind-protect
+       (let* ((new-folders (get-folders db))
+	      (folders-to-watch (set-difference new-folders folders :test #'equal)))
+	 (iter (for folder in folders-to-watch)
+	       (add-watches inotify folder source-queue))
+
+	 new-folders)
+    (socket-close socket)))
