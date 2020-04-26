f8fe8376a144365bfc9102ba06a8a0abf70044aa
Florian Margaine <florian@platform.sh>
2020-04-27 00:08:16 +0200

    Implement querying and adding folder

--- client/main.lisp
+++ client/main.lisp
@@ -9,6 +9,11 @@
 		#:missing-arg
 		#:arg-parser-failed
 		#:missing-required-option)
+  (:import-from :sqlite #:with-open-database #:execute-to-list #:execute-non-query)
+  (:import-from :cl-ppcre #:split)
+  (:import-from :sb-bsd-sockets
+		#:socket
+		#:local-socket)
   (:export #:main))
 
 (in-package :ballish/client/main)
@@ -18,16 +23,24 @@
    :description "print this help text"
    :short #\h
    :long "help")
-  (:name :verbose
-   :description "verbose output"
-   :short #\v
-   :long "verbose")
   (:name :query
    :description "run a query"
    :short #\q
    :long "query"
    :arg-parser #'identity
-   :meta-var "QUERY"))
+   :meta-var "QUERY")
+  (:name :tags
+   :description "tags for the query"
+   :short #\t
+   :long "tags"
+   :arg-parser #'identity
+   :meta-var "TAGS")
+  (:name :folder
+   :description "add folder to index"
+   :short #\f
+   :long "folder"
+   :arg-parser #'identity
+   :meta-var "FOLDER"))
 
 (defun fatal (&rest args)
   (format *error-output* "fatal: ~a~%" (apply #'format nil args))
@@ -60,8 +73,38 @@
        :usage-of "bl")
       (uiop:quit 0))
 
-    (when-option (options :verbose)
-      (format t "Running in verbose mode.~%"))
-
     (when-option (options :query)
-      (format t "Chosen query: ~s~%" (getf options :query)))))
+      (return-from main (query (getf options :query) (getf options :tags))))
+
+    (when-option (options :folder)
+      (return-from main (add-folder (getf options :folder))))))
+
+(defun index-path (&rest more)
+  (uiop:xdg-data-home #p"ballish/" more))
+
+(defun ballish-path (&rest more)
+  (uiop:xdg-data-home #p"ballish/" more))
+
+(defun query (q tags)
+  (with-open-database (db (index-path #p"source.db") :busy-timeout 1000)
+    (format t "~{~a~%~}"
+	    (mapcar
+	     #'car
+	     (if tags
+		 (execute-to-list
+		  db
+		  (format
+		    nil
+		    "SELECT path FROM source WHERE content MATCH ? ~a"
+		    (format nil "~{AND tags MATCH ~s ~}" (split "," tags)))
+		  q)
+		 (execute-to-list db "SELECT path FROM source WHERE content MATCH ?" q))))))
+
+(defun add-folder (folder)
+  (with-open-database (db (ballish-path #p"ballish.db") :busy-timeout 1000)
+    (execute-non-query db "INSERT INTO folder (path) VALUES(?)" folder)
+
+    (let ((socket (make-instance 'local-socket :type :stream)))
+      (sb-bsd-sockets:socket-connect
+       socket
+       (namestring (uiop:xdg-runtime-dir #p"ballish/daemon.sock"))))))
