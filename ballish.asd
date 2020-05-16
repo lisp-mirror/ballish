a637d9675aaa184d8bf3c90c71ce088fdf9c9652
Florian Margaine <florian@platform.sh>
2020-05-17 00:21:10 +0200

    Oh so many things.
    - cleanup .asd, re-enable debug 3
    - handle fatal-error correctly in both REPL and CLI
    - resolve to absolute paths in add-folder
    - handle ballish-daemon not being started
    - handle unique constraint error on existing folder being added again

--- ballish.asd
+++ ballish.asd
@@ -4,9 +4,8 @@
   :class "winfer:wild-package-inferred-system"
   :around-compile (lambda (next)
                     (proclaim '(optimize
-				(debug 1)
+				(debug 3)
 				(safety 1)
-				(debug 1)
 				(speed 3)))
                     (funcall next))
   :depends-on ("ballish/daemon/*")
@@ -19,9 +18,8 @@
   :class "winfer:wild-package-inferred-system"
   :around-compile (lambda (next)
                     (proclaim '(optimize
-				(debug 1)
+				(debug 3)
 				(safety 1)
-				(debug 1)
 				(speed 3)))
                     (funcall next))
   :depends-on ("ballish"
@@ -36,8 +34,7 @@
   ;; libraries cannot be embedded, though, so we need to keep them.
   (uiop:register-image-dump-hook
    (lambda ()
-     (loop
-	for library in (cffi:list-foreign-libraries)
-	unless (eq (cffi:foreign-library-type library) :system)
-	do (cffi:close-foreign-library library))))
+     (dolist (library (cffi:list-foreign-libraries))
+       (when (eql (cffi:foreign-library-type library) :grovel-wrapper)
+	 (cffi:close-foreign-library library)))))
   (setf uiop:*image-entry-point* (asdf/system:component-entry-point c)))
--- client/main.lisp
+++ client/main.lisp
@@ -1,24 +1,29 @@
 (uiop:define-package :ballish/client/main
     (:use :cl :iterate :ballish/util/*)
   (:import-from :unix-opts
-		#:define-opts
-		#:get-opts
-		#:option
-		#:raw-arg
-		#:unknown-option
-		#:missing-arg
-		#:arg-parser-failed
-		#:missing-required-option)
+                #:define-opts
+                #:get-opts
+                #:option
+                #:raw-arg
+                #:unknown-option
+                #:missing-arg
+                #:arg-parser-failed
+                #:missing-required-option)
   (:import-from :cl-cpus #:get-number-of-processors)
-  (:import-from :sqlite #:with-open-database #:execute-to-list #:execute-non-query)
+  (:import-from :sqlite
+                #:with-open-database
+                #:execute-to-list
+                #:execute-non-query
+                #:sqlite-constraint-error)
   (:import-from :cl-ppcre #:split #:regex-replace-all)
   (:import-from :sb-thread #:make-thread #:join-thread)
   (:import-from :sb-concurrency #:make-mailbox #:receive-message #:send-message)
   (:import-from :sb-bsd-sockets
-		#:socket
-		#:local-socket
-		#:socket-connect
-		#:socket-close)
+                #:socket
+                #:local-socket
+                #:socket-connect
+                #:socket-close
+                #:socket-error)
   (:export #:main))
 
 (in-package :ballish/client/main)
@@ -59,104 +64,134 @@
    :short #\o
    :long "optimize"))
 
+(define-condition fatal-error (error)
+  ((message :initarg :message :initform "" :reader message)
+   (code :initarg :code :initform 1 :reader code))
+  (:report (lambda (condition stream)
+             (format stream "fatal: ~a~%" (message condition)))))
+
 (defun fatal (&rest args)
-  (format *error-output* "fatal: ~a~%" (apply #'format nil args))
-  (uiop:quit 1))
+  (error 'fatal-error :message (format nil "~a" (apply #'format nil args))))
 
 (defmacro when-option ((options opt) &body body)
   `(let ((it (getf ,options ,opt)))
      (when it
        ,@body)))
 
+(defmacro handler-bind* (bindings &body body)
+  (expand-handler-binds bindings body))
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defun expand-handler-binds (bindings body)
+    (let ((binding (first bindings)))
+      (if binding
+          `(handler-bind (,binding)
+             ,(expand-handler-binds (rest bindings) body))
+          `(progn ,@body)))))
+
+(defun realpath (path)
+  (check-type path string)
+  (uiop:unix-namestring (uiop:merge-pathnames* (uiop:parse-unix-namestring path))))
+
 (defun main ()
-  (multiple-value-bind (options args)
-      (handler-case
-	  (get-opts)
-	(unknown-option (e)
-	  (fatal "~s option is unknown" (option e)))
-	(missing-arg (e)
-	  (fatal "option ~s needs an argument" (option e)))
-	(arg-parser-failed (e)
-	  (fatal "cannot parse ~s as argument of ~s" (raw-arg e) (option e)))
-	(missing-required-option (e)
-	  (fatal "~a" e)))
+  (handler-bind* ((fatal-error (lambda (c)
+                                (format *error-output* "fatal: ~a~%" (message c))
+                                (uiop:quit (code c))))
+		  (socket-error (lambda (c)
+				  (declare (ignore c))
+				  (fatal "ballish-daemon is not started."))))
+    (multiple-value-bind (options args)
+        (handler-case
+            (get-opts)
+          (unknown-option (e)
+            (fatal "~s option is unknown" (option e)))
+          (missing-arg (e)
+            (fatal "option ~s needs an argument" (option e)))
+          (arg-parser-failed (e)
+            (fatal "cannot parse ~s as argument of ~s" (raw-arg e) (option e)))
+          (missing-required-option (e)
+            (fatal "~a" e)))
 
-    (when args
-      (fatal "unknown parameters: ~{~a~^, ~}" args))
+      (when args
+        (fatal "unknown parameters: ~{~a~^, ~}" args))
 
-    (when-option (options :help)
-      (opts:describe
-       :prefix "a pretty fast code search tool"
-       :usage-of "bl")
-      (uiop:quit 0))
+      (when-option (options :help)
+        (opts:describe
+         :prefix "a pretty fast code search tool"
+         :usage-of "bl")
+        (uiop:quit 0))
 
-    (when-option (options :optimize)
-      (return-from main
-	(progn
-	  (format t "Optimizing storage...~%")
-	  (optimize-fts))))
+      (when-option (options :optimize)
+	(return-from main
+	  (progn
+	    (format t "Optimizing storage...~%")
+	    (optimize-fts))))
 
-    (when-option (options :count)
-      (if (or (getf options :query)
-	      (getf options :tags))
-	  (return-from main
-	    (format t "~{~a~%~}"
-		    (query-count (getf options :query)
-				 (getf options :tags))))
-	  (fatal "cannot count without a query")))
+      (when-option (options :count)
+	(if (or (getf options :query)
+		(getf options :tags))
+	    (return-from main
+	      (format t "~{~a~%~}"
+		      (query-count (getf options :query)
+				   (getf options :tags))))
+	    (fatal "cannot count without a query")))
 
-    (when (or (getf options :query)
-	      (getf options :tags))
-      (return-from main
-	(let ((results (query (getf options :query) (getf options :tags))))
-	  (if (getf options :grep)
-	      (grep (getf options :query) results)
-	      (format t "~{~a~%~}" results)))))
+      (when (or (getf options :query)
+		(getf options :tags))
+	(return-from main
+	  (let ((results (query (getf options :query) (getf options :tags))))
+	    (if (getf options :grep)
+		(grep (getf options :query) results)
+		(format t "~{~a~%~}" results)))))
 
-    (when-option (options :folder)
-      (return-from main (add-folder (getf options :folder))))))
+      (when-option (options :folder)
+	(return-from main
+	  (add-folder (realpath (getf options :folder))))))))
 
 (defun query (q tags &optional (count nil))
   (with-open-database (db (source-index-db-path) :busy-timeout 1000)
     (let ((query
-	   (format
-	    nil
-	    "SELECT ~a FROM source WHERE ~a ~a"
-	    (if count "count(*)" "path")
-	    (if q (format nil "content MATCH ~s" q) "")
-	    (if tags
-		(format nil "~a ~{tags MATCH ~s~^ AND ~}"
-			(if q "AND" "")
-			(split "," tags))
-		""))))
+           (format
+            nil
+            "SELECT ~a FROM source WHERE ~a ~a"
+            (if count "count(*)" "path")
+            (if q (format nil "content MATCH ~s" q) "")
+            (if tags
+                (format nil "~a ~{tags MATCH ~s~^ AND ~}"
+                        (if q "AND" "")
+                        (split "," tags))
+                ""))))
       (mapcar #'car (execute-to-list db query)))))
 
 (defun add-folder (folder)
   (with-open-database (db (ballish-db-path) :busy-timeout 1000)
-    (execute-non-query db "INSERT INTO folder (path) VALUES(?)" folder)
+    (handler-case
+        (execute-non-query db "INSERT INTO folder (path) VALUES(?)" folder)
+      (sqlite-constraint-error ()
+        (fatal "folder ~a is already indexed." folder)))
 
     (let ((socket (make-instance 'local-socket :type :stream)))
       (unwind-protect
-	   (socket-connect
-	    socket
-	    (namestring (uiop:xdg-runtime-dir #p"ballish/daemon.sock")))
-	(socket-close socket)))))
+           (socket-connect
+            socket
+            (namestring (uiop:xdg-runtime-dir #p"ballish/daemon.sock")))
+        (socket-close socket)))))
 
 (defun query-count (q tags)
   (query q tags t))
 
 (defun grep (query results)
   (when (> (length results) (or (and (uiop:getenv "BL_MAX_GREP_RESULTS")
-				     (parse-integer (uiop:getenv "BL_MAX_GREP_RESULTS")))
-				100))
+                                     (parse-integer (uiop:getenv "BL_MAX_GREP_RESULTS")))
+                                100))
     (fatal "too many results to grep."))
   (let ((search (regex-replace-all "(\\w+)"
-				   (regex-replace-all "[\\+\\s]+" query ".*")
-				   "\\b\\1\\b")))
+                                   (regex-replace-all "[\\+\\s]+" query ".*")
+                                   "\\b\\1\\b")))
     (handler-case
-	(uiop:run-program
-	 (format nil "grep -HPins ~s ~{~s~^ ~}" search results)
-	 :output :interactive :error-output :interactive :input :interactive)
+        (uiop:run-program
+         (format nil "grep -HPins ~s ~{~s~^ ~}" search results)
+         :output :interactive :error-output :interactive :input :interactive)
       (error (e) (format *error-output* "~a~%" e)))))
 
 (defun optimize-fts ()
