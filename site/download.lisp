3587802218ad7fd2445aa6e9b03c74bda65ade13
Florian Margaine <florian@platform.sh>
2020-07-08 01:58:11 +0200

    Refactor download page.
    Turns out Lisp code is handy.

--- site/download.lisp
+++ site/download.lisp
@@ -13,7 +13,7 @@
 
      (:p (format
 	  nil
-	  "The packages for ballish ~a can be downloaded on Gitlab.com: "
+	  "The packages for latest ballish release can be downloaded on Gitlab.com: "
 	  *version*)
 	 (:a :href (format
 		    nil
@@ -41,48 +41,39 @@
 	      (:li (:a :href (format nil "#fedora-~a" version)
 		       (format nil "Fedora ~a" version)))))))
 
-     (:h3 :id "arch" "Arch Linux")
+     (flet ((instructions (file command)
+	      (:p "Download the "
+		  (:code file)
+		  " file, then install the package with:")
+	      (:pre (format nil "~a ~a" command file))
+	      (:p "Then enable and start the daemon with:")
+	      (:pre "sudo systemctl enable --now ballish-daemon@\"$USER\"")
+	      (:p "You can then look at " (:code "man ballish")
+		  ", with the first command to look at being " (:code "bl --folder"))))
 
-     (:p "Download the "
-	 (:code (format nil "ballish-~a-1-x86_64.pkg.tar.xz" *version*))
-	 " file, then install the package with:")
-     (:pre (format nil "sudo pacman -U ballish-~a-1-x86_64.pkg.tar.xz" *version*))
-     (:p "Then enable and start the daemon with:")
-     (:pre "sudo systemctl enable --now ballish-daemon@\"$USER\"")
+       (:h3 :id "arch" "Arch Linux")
 
-     (:h3 :id "debian" "Debian")
+       (instructions (format nil "ballish-~a-1-x86_64.pkg.tar.xz" *version*)
+		     "sudo pacman -U")
 
-     (dolist (distribution '("stretch" "buster" "bullseye"))
-       (let ((file (format nil "ballish_~a_~a_amd64.deb" distribution *version*)))
-	 (:h4 :id (format nil "debian-~a" distribution)
-	      (format nil "Debian ~a" (string-capitalize distribution)))
-	 (:p "Download the "
-	     (:code file)
-	     " file, then install the package with:")
-	 (:pre (format nil "sudo dpkg -i ~a" file))
-	 (:p "Then enable and start the daemon with:")
-	 (:pre "sudo systemctl enable --now ballish-daemon@\"$USER\"")))
+       (:h3 :id "debian" "Debian")
 
-     (:h3 :id "ubuntu" "Ubuntu")
+       (dolist (distribution '("stretch" "buster" "bullseye"))
+	 (let ((file (format nil "ballish_~a_~a_amd64.deb" distribution *version*)))
+	   (:h4 :id (format nil "debian-~a" distribution)
+		(format nil "Debian ~a" (string-capitalize distribution)))
+	   (instructions file "sudo dpkg -i")))
 
-     (dolist (version '("18.04" "19.10" "20.04"))
-       (let ((file (format nil "ballish_ubuntu_~a_~a_amd64.deb" version *version*)))
-	 (:h4 :id (format nil "ubuntu-~a" version) (format nil "Ubuntu ~a" version))
-	 (:p "Download the "
-	     (:code file)
-	     " file, then install the package with:")
-	 (:pre (format nil "sudo dpkg -i ~a" file))
-	 (:p "Then enable and start the daemon with:")
-	 (:pre "sudo systemctl enable --now ballish-daemon@\"$USER\"")))
+       (:h3 :id "ubuntu" "Ubuntu")
 
-     (:h3 :id "fedora" "Fedora")
+       (dolist (version '("18.04" "19.10" "20.04"))
+	 (let ((file (format nil "ballish_ubuntu_~a_~a_amd64.deb" version *version*)))
+	   (:h4 :id (format nil "ubuntu-~a" version) (format nil "Ubuntu ~a" version))
+	   (instructions file "sudo dpkg -i")))
 
-     (dolist (version '("31" "32"))
-       (let ((file (format nil "ballish-~a-1.f~a.x86_64.rpm" *version* version)))
-	 (:h4 :id (format nil "fedora-~a" version) (format nil "Fedora ~a" version))
-	 (:p "Download the "
-	     (:code file)
-	     " file, then install the package with:")
-	 (:pre (format nil "sudo dnf localinstall ~a" file))
-	 (:p "Then enable and start the daemon with:")
-	 (:pre "sudo systemctl enable --now ballish-daemon@\"$USER\""))))))
+       (:h3 :id "fedora" "Fedora")
+
+       (dolist (version '("31" "32"))
+	 (let ((file (format nil "ballish-~a-1.f~a.x86_64.rpm" *version* version)))
+	   (:h4 :id (format nil "fedora-~a" version) (format nil "Fedora ~a" version))
+	   (instructions file "sudo dnf localinstall")))))))
